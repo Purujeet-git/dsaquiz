efe634728a0c3ee04edd1b9a2fd71da6424b2df6
{Purujeet} <purujeetkr2005@gmail.com>
2026-01-31 01:57:17 +0530

    Remove unused folders

--- app/api/quizzes/route.js
+++
@@ -1,8 +0,0 @@
-import { connectDB } from "@/lib/db";
-import Quiz from "@/models/Quiz";
-
-export async function GET() {
-  await connectDB();
-  const quizzes = await Quiz.find({}, { quizTag: 1, _id: 0 });
-  return Response.json(quizzes);
-}
